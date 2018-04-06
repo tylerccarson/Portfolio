107eac3e02d0b49ca7222a09cfde6274b6350941
tyler carson <tyler.c.carson@gmail.com>
2018-04-06 11:57:25 -0700

    remove backend

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-let port = process.env.PORT || 3000;
-
-app.use(express.static('public'));
-
-app.listen(port, () => {
-  console.log('Listening on port ' + port + '...');
-});
